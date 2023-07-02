3da122e3c70bd4158a5c8569978a0b65450a48d5
Yeonseo-Jo <jys073190@gmail.com>
2023-07-03 00:11:12 +0900

    init : 초기 구조

--- yeonseo-pretask/src/Router.tsx
+++ yeonseo-pretask/src/Router.tsx
@@ -7,6 +7,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/carousel" element={<Carousel />} />
       </Routes>
     </BrowserRouter>
   );
